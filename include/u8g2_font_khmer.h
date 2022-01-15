35bd51be7d8a43482c7cd3d190a11017200850fd
Sovichea Tep <sovichea.tep@gmail.com>
2022-01-15 15:55:12 +0700

    Updated Makefile

--- include/u8g2_font_khmer.h
+++ include/u8g2_font_khmer.h
@@ -4,7 +4,7 @@
 #include "u8g2.h"
 
 extern const uint8_t notosans_6x8[2585] U8G2_FONT_SECTION("notosans_6x8");
-extern const uint8_t notosans_6x9[2585] U8G2_FONT_SECTION("notosans_6x9");
+extern const uint8_t notosans_6x9[2659] U8G2_FONT_SECTION("notosans_6x9");
 
 u8g2_uint_t u8g2_DrawKhmer(u8g2_t *u8g2, u8g2_uint_t x, u8g2_uint_t y, const char *str);
 
