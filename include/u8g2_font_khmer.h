1aa7c4c3c449996f0c9cdb0e546c376249ce841a
Sovichea Tep <sovichea.tep@gmail.com>
2022-01-15 15:50:28 +0700

    Updated filename

--- include/u8g2_font_khmer.h
+++ include/u8g2_font_khmer.h
@@ -1,5 +1,5 @@
-#ifndef SHAPE_KHMER_H_
-#define SHAPE_KHMER_H_
+#ifndef U8G2_FONT_KHMER_H_
+#define U8G2_FONT_KHMER_H_
 
 #include "u8g2.h"
 
@@ -8,4 +8,4 @@ extern const uint8_t notosans_6x9[2585] U8G2_FONT_SECTION("notosans_6x9");
 
 u8g2_uint_t u8g2_DrawKhmer(u8g2_t *u8g2, u8g2_uint_t x, u8g2_uint_t y, const char *str);
 
-#endif // SHAPE_KHMER_H_
+#endif // U8G2_FONT_KHMER_H_
