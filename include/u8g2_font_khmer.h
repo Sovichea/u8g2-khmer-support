7eebef90b35b110e45d8f9d0ba839a1e1801f3d4
Sovichea Tep <sovichea.tep@gmail.com>
2022-03-04 16:48:32 +0700

    Added new font

--- include/u8g2_font_khmer.h
+++ include/u8g2_font_khmer.h
@@ -3,6 +3,8 @@
 
 #include "u8g2.h"
 
+extern const uint8_t waffle_10[] U8G2_FONT_SECTION(waffle_10);
+extern const uint8_t waffle-10[] U8G2_FONT_SECTION(waffle-10);
 extern const uint8_t u8g2KHTest_R16[] U8G2_FONT_SECTION(u8g2KHTest_R16);
 extern const uint8_t notosans_6x9[] U8G2_FONT_SECTION(notosans_6x9);
 extern const uint8_t notosans_6x8[] U8G2_FONT_SECTION(notosans_6x8);
