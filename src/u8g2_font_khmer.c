58ff43c54b8a7f53af78eb94b4ea818f754a6054
Sovichea TEP <sovichea.tep@gmail.com>
2023-02-10 14:27:03 +0700

    Update algorithm

--- src/u8g2_font_khmer.c
+++ src/u8g2_font_khmer.c
@@ -2,9 +2,8 @@
 #include <string.h>
 #include "u8g2_font_khmer.h"
 
-#define GLYPH_BASE_START 0x1780
-#define GLYPH_BASE_STOP  0x17B3
-#define GLYPH_GET_ORD(x) x - GLYPH_BASE_START
+#define GLYPH_START 0x1780
+#define GLYPH_GET_ORD(x) x - GLYPH_START
 #define GLYPH_GET_BASE(x) x - 0xE000 + 0x1780
 #define GLYPH_GET_BASE_SUB(b) 0xE000 + GLYPH_GET_ORD(b)
 #define GLYPH_GET_BASE_SUB2(b) 0xE050 + GLYPH_GET_ORD(b)
@@ -17,6 +16,7 @@ enum {
   CAT_COENG_RO,
   CAT_BASE,
   CAT_COENG,
+  CAT_COENG_POST,
   CAT_SHIFT,
   CAT_VB,
   CAT_VA,
@@ -61,12 +61,11 @@ static void log_unicode_array(struct unicode_t *buf, int size)
 {
   for (int i = 0; i < size; ++i)
   {
-    if (buf[i].encoding > 0)
-    {
-      printf("0x%x (%d), ", buf[i].encoding, buf[i].charcat);
+    if (buf[i].encoding > 0) {
+      //printf("0x%x (%d), ", buf[i].encoding, buf[i].charcat);
     }
   }
-  printf("\n");
+  //printf("\n");
 }
 
 static int data_cmp(const void *a, const void *b)
@@ -76,21 +75,11 @@ static int data_cmp(const void *a, const void *b)
   return da->charcat < db->charcat ? -1 : da->charcat > db->charcat;
 }
 
-static uint8_t is_base_glyph(uint16_t u)
-{
-  return (u >= GLYPH_BASE_START && u <= GLYPH_BASE_STOP);
-}
-
-static uint8_t is_other_glyph(uint16_t u)
-{
-  return (u >= 0x17D4 && u <= 0x17E9);
-}
-
 static u8g2_uint_t u8g2_draw_string_khmer(u8g2_t *u8g2, u8g2_uint_t x, u8g2_uint_t y, const char *str)
 {
   uint16_t e, e_prev = 0;
-  uint16_t e_buf_in[80] = {0};
-  uint16_t e_buf_out[80] = {0};
+  uint16_t e_buf_in[1024] = {0};
+  uint16_t e_buf_out[1024] = {0};
   uint16_t * in_ptr; 
   uint16_t * out_ptr;
   u8g2_uint_t delta, sum;
@@ -102,13 +91,13 @@ static u8g2_uint_t u8g2_draw_string_khmer(u8g2_t *u8g2, u8g2_uint_t x, u8g2_uint
   {
     e = u8g2->u8x8.next_cb(u8g2_GetU8x8(u8g2), (uint8_t)*str);
  
-    if ( e == 0x0ffff )
-    {
+    if ( e == 0x0ffff ) {
       break;
     }
+
     str++;
-    if ( e != 0x0fffe)
-    {
+    
+    if ( e != 0x0fffe) {
       *(in_ptr++) = e;
     }
   }
@@ -117,14 +106,17 @@ static u8g2_uint_t u8g2_draw_string_khmer(u8g2_t *u8g2, u8g2_uint_t x, u8g2_uint
   while (*(in_ptr) != 0)
   {
     // start reading khmer consonant cluster
+    int has_coeng = 0;
+    int has_coeng_post = 0;
+    int has_aa = 0;
+    int has_va = 0;
     int idx = 0;
     struct unicode_t buf[10] = {0};
     int g = *(in_ptr++);
-    if (g < 255 || CAT(g) == CAT_OTHER)
-    {
+    if (g < 255 || CAT(g) == CAT_OTHER) {
       *(out_ptr++) = g;
-      printf("0x%x\n", g);
-      printf("\n");
+      //printf("0x%x\n", g);
+      //printf("\n");
       continue;
     }
 
@@ -133,175 +125,171 @@ static u8g2_uint_t u8g2_draw_string_khmer(u8g2_t *u8g2, u8g2_uint_t x, u8g2_uint
     while(1)
     {
       g = *(in_ptr++);
-      if (g < 255 || CAT(g) == CAT_OTHER)
-      {
+      if (g < 255 || CAT(g) == CAT_OTHER) {
         in_ptr--;
         break;
       }
-      else if (CAT(g_prev) != CAT_COENG && CAT(g) == CAT_BASE)
-      {
+      else if (CAT(g_prev) != CAT_COENG && CAT(g) == CAT_BASE) {
         in_ptr--;
         break;
       }
 
       // add 0x17C1 (េ) for all VPOST glyph
-      if (CAT(g) == CAT_VPOST && g != 0x17B6)
+      if (CAT(g) == CAT_VPOST && g != 0x17B6) {
         buf[idx++].encoding = 0x17C1;
+      }
+
+      if (g == 0x17C6 || CAT(g) == CAT_VA) has_va = 1;
 
       buf[idx++].encoding = g;
       g_prev = g;
     }
 
-    if (idx == 1) { 
+    if (idx == 1) {
       *(out_ptr++) = buf[0].encoding;
-      printf("0x%x (%d)\n", buf[0].encoding, CAT(buf[0].encoding));
-      printf("\n");
+      //printf("0x%x (%d)\n", buf[0].encoding, CAT(buf[0].encoding));
+      //printf("\n");
       continue;
     }
 
     // start shaping
-
+  
     // create a category map
     for (int i = 0; i < sizeof(buf)/sizeof(*buf); ++i)
     {
       buf[i].charcat = CAT_OTHER;
-      if (buf[i].encoding > 0)
-      {
+      if (buf[i].encoding > 0) {
         // substitute COENG and COENG RO then remove 0x17D2
         buf[i].charcat = CAT(buf[i].encoding);
-        if (i > 0 && buf[i-1].encoding == 0x17D2)
-        {
-          if (buf[i].encoding == 0x179A) buf[i].charcat = CAT_COENG_RO;
+        if (i > 0 && buf[i-1].encoding == 0x17D2) {
+          if (buf[i].encoding != 0x179A) has_coeng = 1;
           buf[i].encoding = GLYPH_GET_BASE_SUB(buf[i].encoding);
+          buf[i].charcat = CAT_COENG;
           buf[i-1].encoding = 0;
           buf[i-1].charcat = CAT_OTHER;
+        }
+
+        if (buf[i].encoding == 0x1789 || buf[i].encoding == 0x17A1)
+        {
+          has_coeng = 1;
         }
       }
     }
 
+    // first sort to push all encoding with 0 values to the end of array
+    qsort(buf, sizeof(buf)/sizeof(*buf), sizeof(*buf), data_cmp);
+
+    // apply feature for COENG RO and COENG POST
+    for (int i = 1; i < sizeof(buf)/sizeof(*buf); ++i)
+    {
+      // reposition COENG RO
+      if (buf[i].encoding == GLYPH_GET_BASE_SUB(0x179A)) {
+        buf[i].charcat = CAT_COENG_RO;
+      }
+      
+      if (buf[i].encoding == GLYPH_GET_BASE_SUB(0X1783) ||
+          buf[i].encoding == GLYPH_GET_BASE_SUB(0X1788) ||
+          buf[i].encoding == GLYPH_GET_BASE_SUB(0X1783) ||
+          buf[i].encoding == GLYPH_GET_BASE_SUB(0X1799))
+      {
+        buf[i].charcat = CAT_COENG_POST;
+        has_coeng_post = 1;
+      }
+      // count the number of COENG and AA
+      if (buf[i].encoding == 0x17B6 || buf[i].encoding == 0x17C4 || buf[i].encoding == 0x17C5) has_aa = 1;
+    }
+
     for (int i = 0; i < sizeof(buf)/sizeof(*buf); ++i)
     {
-      if (buf[i].encoding > 0)
-      {
-        if (buf[i].encoding == 0x17B7 && buf[i+1].encoding == 0x17CD)
-        {
-          buf[i].encoding = 0xE030;
-          buf[i+1].encoding = 0;
-          buf[i+1].charcat = CAT_OTHER;
+      if (buf[i].encoding > 0) {
+
+        // apply second substitute feature
+        if (buf[i].charcat == CAT_COENG_RO && has_coeng && !has_coeng_post) {
+          if (buf[i-2].encoding != 0x179F && buf[i-2].encoding != 0x1799 && buf[i-1].encoding != 0x178D && buf[i-1].encoding != 0x178E && 
+              buf[i-2].encoding != 0x179B && buf[i-2].encoding != 0x17A0 && buf[i-1].encoding != 0x1783 && buf[i-1].encoding != 0x1788 &&
+              buf[i-2].encoding != 0x1789)
+          {
+            buf[i].encoding = GLYPH_GET_BASE_SUB2(GLYPH_GET_BASE(buf[i].encoding));
+          }
+        }
+        else if (buf[i].charcat == CAT_COENG && buf[i-1].charcat == CAT_BASE) {
+
+        }
+
+        if (buf[i].charcat == CAT_COENG_POST && buf[i-1].charcat == CAT_COENG) {
+          buf[i].encoding = GLYPH_GET_BASE_SUB2(GLYPH_GET_BASE(buf[i].encoding));
         }
 
         // apply feature for RO
-        if (buf[0].encoding == 0x179A)  
-        {
-          if (CAT(buf[i].encoding) == CAT_VA || buf[i].encoding == 0x17C6 || 
-              buf[i].encoding == 0x17C9)
-          {
-            buf[i].encoding = GLYPH_GET_RO_FEATURE(buf[i].encoding);
+        if (buf[i].encoding == 0x179A || buf[i].encoding == 0x179C) {
+          if (buf[i+1].charcat == CAT_VA || buf[i+1].encoding == 0x17C6 || buf[i+1].encoding == 0x17C9) {
+            buf[i+1].encoding = GLYPH_GET_RO_FEATURE(buf[i+1].encoding);
           }
         }
 
         // apply feature for TREYSAP
-        if (buf[0].encoding == 0x1794 && buf[i].encoding == 0x17CA)
-        {
+        if (buf[i].encoding == 0x1794 && buf[i+1].encoding == 0x17CA) {
           i++;
-          buf[i].encoding = GLYPH_GET_TREYSAP_FEATURE(buf[i].encoding);
-          continue;
+          buf[i+1].encoding = GLYPH_GET_TREYSAP_FEATURE(buf[i+1].encoding);
         }
-
         // apply reg shift feature
-        if (buf[i].encoding == 0x17C9 || buf[i].encoding == 0x17CA)
-        {
-          if (buf[i+1].charcat == CAT_VA)
-          {
+        else if (buf[i].encoding == 0x17C9 || buf[i].encoding == 0x17CA) {
+          if (has_va) {
             buf[i].encoding = 0x17BB;
             buf[i].charcat = CAT(buf[i].encoding); 
           }
         }
       }
     }
-    printf("before sort: "); log_unicode_array(buf, sizeof(buf)/sizeof(*buf));
+    //printf("before sort: "); log_unicode_array(buf, sizeof(buf)/sizeof(*buf));
     qsort(buf, sizeof(buf)/sizeof(*buf), sizeof(*buf), data_cmp);
-    printf("after sort: "); log_unicode_array(buf, sizeof(buf)/sizeof(*buf));
-    printf("\n");
+    //printf("after sort: "); log_unicode_array(buf, sizeof(buf)/sizeof(*buf));
+    //printf("\n");
 
     // apply ligature
     for (int i = 0; i < sizeof(buf)/sizeof(*buf); ++i)
     {
-      if (buf[i].encoding > 0)
-      {
+      if (buf[i].encoding > 0) {
+        // apply ិ៍ feature
+        if (buf[i].encoding == 0x17B7 && buf[i+1].encoding == 0x17CD) {
+          buf[i].encoding = 0xE030;
+          buf[i+1].encoding = 0;
+          buf[i+1].charcat = CAT_OTHER;
+        }
+
         // apply BA feature
-        if (buf[i].encoding == 0x1794)
-        {
-          if (buf[i+1].encoding == 0x17C9 && buf[i+2].encoding == 0x17B6)
-          {
-            buf[i].encoding = 0x17EA;
-          }
-          else if (buf[i+1].encoding == 0x17B6 || 
-              buf[i+1].encoding == 0x17C4 || 
-              buf[i+1].encoding == 0x17C5)
-          {
-            buf[i].encoding = 0x17EA;
-          }
-          else if (buf[i+2].encoding == 0x17B6 || 
-              buf[i+2].encoding == 0x17C4 || 
-              buf[i+2].encoding == 0x17C5)
-          {
-            if (buf[i+1].charcat == CAT_BASE &&
-                GLYPH_GET_BASE(buf[i+1].encoding) != 0x1783 &&
-                GLYPH_GET_BASE(buf[i+1].encoding) != 0x1788 &&
-                GLYPH_GET_BASE(buf[i+1].encoding) != 0x178D &&
-                GLYPH_GET_BASE(buf[i+1].encoding) != 0x1794 &&
-                GLYPH_GET_BASE(buf[i+1].encoding) != 0x1799 &&
-                GLYPH_GET_BASE(buf[i+1].encoding) != 0x179F)
-              {
-                buf[i].encoding = 0x17EA;
-              }
-          }
+        if (buf[i].encoding == 0x1794 && has_aa && !has_coeng_post) {
+          buf[i].encoding = 0x17EA;
         }
 
         // apply NHO feature
-        if (buf[i].encoding == 0x1789)
-        {
-          if (is_base_glyph(GLYPH_GET_BASE(buf[i+1].encoding)))
-          {
+        if (buf[i].encoding == 0x1789) {
+          if (buf[i+1].charcat == CAT_COENG || buf[i+1].charcat == CAT_COENG_POST) {
             buf[i].encoding = 0x17EB;
           }
-          if (GLYPH_GET_BASE(buf[i+1].encoding) == 0x1789)
-          {
+          if (GLYPH_GET_BASE(buf[i+1].encoding) == 0x1789) {
             buf[i+1].encoding = GLYPH_GET_BASE_SUB2(0x1789);
           }
         }
 
-        // apply second substitute feature
-        if (is_base_glyph(GLYPH_GET_BASE(buf[i].encoding)) && 
-            is_base_glyph(GLYPH_GET_BASE(buf[i+1].encoding)))
+        // apply second vowel feature
+        if (buf[i].charcat == CAT_VPOST && has_coeng)
         {
-          if (GLYPH_GET_BASE(buf[i].encoding) != 0x179F &&
-              GLYPH_GET_BASE(buf[i].encoding) != 0x1799 &&
-              GLYPH_GET_BASE(buf[i].encoding) != 0x178D &&
-              GLYPH_GET_BASE(buf[i].encoding) != 0x178E &&
-              GLYPH_GET_BASE(buf[i].encoding) != 0x179B &&
-              GLYPH_GET_BASE(buf[i].encoding) != 0x17A0 &&
-              GLYPH_GET_BASE(buf[i].encoding) != 0x1783 && 
-              GLYPH_GET_BASE(buf[i].encoding) != 0x1788 &&
-              GLYPH_GET_BASE(buf[i].encoding) != 0x1789)
-          {
-            buf[i+1].encoding = GLYPH_GET_BASE_SUB2(buf[i+1].encoding);
+          if (buf[i].encoding == 0x17BF || buf[i].encoding == 0x17C0) {
+            buf[i].encoding = GLYPH_GET_VOWEL_SUB(buf[i].encoding);
           }
         }
 
-        // apply second vowel feature
-        if(is_base_glyph(GLYPH_GET_BASE(buf[i].encoding)) && buf[i+1].charcat == CAT_VB)
+        if (buf[i].charcat == CAT_VB && has_coeng)
         {
-          buf[i+1].encoding = GLYPH_GET_VOWEL_SUB(buf[i+1].encoding);
+          buf[i].encoding = GLYPH_GET_VOWEL_SUB(buf[i].encoding);
         }
       }
     }
 
     for (int i = 0; i < sizeof(buf)/sizeof(*buf); ++i)
     {
-      if (buf[i].encoding > 0)
-      {
+      if (buf[i].encoding > 0) {
         *(out_ptr++) = buf[i].encoding;
       }
     }
